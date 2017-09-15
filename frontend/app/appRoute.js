cca10adad0647ed0db4bea114b4fbddcaa4794a2
projectvelox <jojo.ileadtrainings@gmail.com>
2017-09-15 11:33:17 +0800

    added user folder and initialized user dashboard

--- frontend/app/appRoute.js
+++ frontend/app/appRoute.js
@@ -6,16 +6,32 @@ angular.module('appRoute', ['ngRoute'])
             //PUBLIC
             .when('/', {
                 templateUrl: 'app/pages/public/home.html'
+                //,
+                //controller: '',
+                //controllerAs: ''
             })
 
             .when('/signup', {
                 templateUrl: 'app/pages/public/signup.html'
+                //,
+                //controller: '',
+                //controllerAs: ''
             })
 
             .when('/login', {
                 templateUrl: 'app/pages/public/login.html'
+                //,
+                //controller: '',
+                //controllerAs: ''
             })
 
+            //USERS
+            .when('/user/dashboard', {
+                templateUrl: 'app/pages/user/dashboard.html'
+                //,
+                //controller: '',
+                //controllerAs: ''
+            })
 
             .otherwise({redirectTo: '/'});
     });
