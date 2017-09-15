fc6fa9100b30499e2f44343f15121b83a1d2270f
Makoy <neimarkbraga@gmail.com>
2017-09-15 10:35:45 +0800

    added home page

--- frontend/app/appRoute.js
+++ frontend/app/appRoute.js
@@ -2,5 +2,11 @@ angular.module('appRoute', ['ngRoute'])
     .config(function ($routeProvider, $locationProvider) {
         $locationProvider.hashPrefix('');
         $routeProvider
+
+            //PUBLIC
+            .when('/', {
+                templateUrl: 'app/pages/public/home.html'
+            })
+
             .otherwise({redirectTo: '/'});
     });
